fbd5d55184647940afc21fda9809104d98f3a35b
Ratul-Tester <ratul.tester@gmail.com>
2024-07-16 12:25:48 +0530

    Ping example added

--- src/test/java/com/thetestingaccademy/ex_ra01/Lab260.java
+++ src/test/java/com/thetestingaccademy/ex_ra01/Lab260.java
@@ -1,5 +1,14 @@
 package com.thetestingaccademy.ex_ra01;
 
-public class Lab260 {
+import io.restassured.RestAssured;
 
+public class Lab260 {
+    public static void main (String[] args){
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when().get()
+                .then().statusCode(201);
+    }
 }
