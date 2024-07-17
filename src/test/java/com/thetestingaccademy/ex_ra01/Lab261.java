84807cc34cc0b79f9abf636a0b3281107b08463f
Ratul-Tester <ratul.tester@gmail.com>
2024-07-17 09:15:31 +0530

    New report and files added

--- src/test/java/com/thetestingaccademy/ex_ra01/Lab261.java
+++ src/test/java/com/thetestingaccademy/ex_ra01/Lab261.java
@@ -3,7 +3,7 @@ package com.thetestingaccademy.ex_ra01;
 import io.restassured.RestAssured;
 
 public class Lab261 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         RestAssured
                 .given()
                 .baseUri("https://restful-booker.herokuapp.com")
