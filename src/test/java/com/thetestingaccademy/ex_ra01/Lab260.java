772bf0d31ce2be7d1c70770d09e933b707cb52fa
Ratul-Tester <ratul.tester@gmail.com>
2024-07-18 12:08:36 +0530

    New result added

--- src/test/java/com/thetestingaccademy/ex_ra01/Lab260.java
+++ src/test/java/com/thetestingaccademy/ex_ra01/Lab260.java
@@ -1,9 +1,15 @@
 package com.thetestingaccademy.ex_ra01;
 
+import io.qameta.allure.Description;
 import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
 
 public class Lab260 {
-    public static void main(String[] args) {
+    @Description("Checking for ping module")
+    @Test
+    public void pingModule()
+   /*public static void main(String[] args) */{
         RestAssured
                 .given()
                 .baseUri("https://restful-booker.herokuapp.com")
--- src/test/java/com/thetestingaccademy/ex_ra01/Lab261.java
+++ src/test/java/com/thetestingaccademy/ex_ra01/Lab261.java
@@ -1,9 +1,16 @@
 package com.thetestingaccademy.ex_ra01;
 
+import io.qameta.allure.Description;
 import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+
 
 public class Lab261 {
-    public static void main(String[] args) {
+    @Description("Checking for booking module")
+    @Test
+    public void bookingModule()
+    /*public static void main(String[] args) */{
         RestAssured
                 .given()
                 .baseUri("https://restful-booker.herokuapp.com")
